7da9b643ad0bb883f7b4ab337758dae41dcc0c78
JMasalski <masalski.kuba123@gmail.com>
2024-11-28 17:50:40 +0100

    Modifed header, added menu

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ document.addEventListener('DOMContentLoaded', function () {
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 });
+
+window.onload = () => {
+    menu();
+};
+
+function menu() {
+    const dane = document.querySelector('#dane'); // Define the dane variable
+
+    let lista = "<ul class='lista rounded my-5'>";
+    fetch('menu.json')
+        .then(response => response.json())
+        .then(data => {
+            const listaMenu = data;
+            listaMenu.forEach(pizza => {
+                console.log(`Składniki dla ${pizza.nazwa_pizzy}:`);
+                pizza.skladniki.forEach(skladnik => {
+                    console.log(`- ${skladnik}`);
+                });
+                lista += `<li class='d-flex gap-2 align-items-center list-group-item'><h2>${pizza.nazwa_pizzy}</h2> - ${pizza.skladniki.join(', ')} 
+<button type='button' class='btn mx-2 ms-auto'>${pizza.cena} zł</button></li>`;
+            });
+            // Add customizable pizza
+            lista += `<li class='d-flex gap-2 align-items-center list-group-item'><h2>Stwórz własną pizzę</h2> - Wybierz własne składniki 
+<button type='button' class='btn mx-2 ms-auto'>Cena zależna od składników</button></li>`;
+            dane.innerHTML = lista + "</ul>";
+        })
+        .catch(error => console.error('Błąd podczas wczytywania menu:', error));
+}
